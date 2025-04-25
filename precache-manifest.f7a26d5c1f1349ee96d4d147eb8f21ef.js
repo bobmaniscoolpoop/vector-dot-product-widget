25af048f31a69d97fc1f4f7e5f69c83dcee9363f
bobmaniscoolpoop <eric2018kc@gmail.com>
2025-04-25 11:42:03 -0700

    Updates

---
+++ precache-manifest.f7a26d5c1f1349ee96d4d147eb8f21ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbbad1712d32e196dba806731177aaf",
+    "url": "/vector-dot-product-widget/index.html"
+  },
+  {
+    "revision": "fd30e1d762581764b3e8",
+    "url": "/vector-dot-product-widget/static/css/main.8280d99b.chunk.css"
+  },
+  {
+    "revision": "da8b8ee81106c8324f14",
+    "url": "/vector-dot-product-widget/static/js/2.4519393a.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/vector-dot-product-widget/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "fd30e1d762581764b3e8",
+    "url": "/vector-dot-product-widget/static/js/main.e4533a43.chunk.js"
+  },
+  {
+    "revision": "e2c9842719c92f306b21",
+    "url": "/vector-dot-product-widget/static/js/runtime~main.6f41b8f1.js"
+  }
+]);
